8d91626465c5714ab856f088e07e8641c8a44a35
Mikhail Dmitrievskiy <mdmitr@gmail.com>
2015-12-22 07:18:44 +0500

    capturing camera image

--- app/ThermalScan.py
+++ app/ThermalScan.py
@@ -13,11 +13,14 @@ class Ui_ThermalScanDialog(QtWidgets.QDialog, ui_ThermalScan.Ui_ThermalScanDialo
 		self.setupUi(self)
 		self.pushButton_Calibrate.clicked.connect(self.handle_Calibrate)
 		self.pushButton_Close.clicked.connect(self.handle_Close)
+		self.pushButton_Scan.clicked.connect(self.handle_Scan)
 
 		image_file = r'../image_processing/edge_detection/pic3.jpg'
 		self.cvImage = cv2.imread(image_file)
 		height, width, bytesPerComponent = self.cvImage.shape
 		bytesPerLine = bytesPerComponent * width
+
+		self.cap = cv2.VideoCapture(0)
 
 		cv2.cvtColor(self.cvImage, cv2.COLOR_BGR2RGB, self.cvImage)
 		self.mQImage = QImage(self.cvImage, width, height, bytesPerLine, QImage.Format_RGB888)
@@ -30,7 +33,21 @@ class Ui_ThermalScanDialog(QtWidgets.QDialog, ui_ThermalScan.Ui_ThermalScanDialo
 		hh = self.mainImage.height()-2
 		self.mainImage.setPixmap(self.mQPixmap.scaled(ww, hh, Qt.KeepAspectRatio))
 
-	def ScanButtonPressed(self, param):
+	def handle_Scan(self, param):
+		
+		ret, frame = self.cap.read()
+		print(ret)
+		self.cvImage = frame
+		height, width, bytesPerComponent = self.cvImage.shape
+		bytesPerLine = bytesPerComponent * width
+
+		cv2.cvtColor(self.cvImage, cv2.COLOR_BGR2RGB, self.cvImage)
+		self.mQImage = QImage(self.cvImage, width, height, bytesPerLine, QImage.Format_RGB888)
+		self.mQPixmap = QPixmap.fromImage(self.mQImage)
+		ww = self.mainImage.width()-2
+		hh = self.mainImage.height()-2
+		self.mainImage.setPixmap(self.mQPixmap.scaled(ww, hh, Qt.KeepAspectRatio))		
+		self.mainImage.repaint()
 		pass
 
 	def handle_Calibrate(self):
