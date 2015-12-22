1adfe076b6743a88b2a5ce38027413613c2b4eed
Mikhail Dmitrievskiy <mdmitr@gmail.com>
2015-12-22 06:36:37 +0500

    calibrate handle added

--- app/ThermalScan.py
+++ app/ThermalScan.py
@@ -1,6 +1,8 @@
 from PyQt5.QtCore import QObject, pyqtSignal
 from PyQt5 import QtWidgets
 
+import cv2
+
 import ui_ThermalScan
 
 class Ui_ThermalScanDialog(QtWidgets.QDialog, ui_ThermalScan.Ui_ThermalScanDialog):
@@ -8,11 +10,14 @@ class Ui_ThermalScanDialog(QtWidgets.QDialog, ui_ThermalScan.Ui_ThermalScanDialo
 	def __init__(self, parent=None):
 		super(Ui_ThermalScanDialog, self).__init__(parent)
 		self.setupUi(self)
-		print("dsfsdfsdf")
+		self.pushButton_Calibrate.clicked.connect(self.handle_Calibrate)
 		self.pushButton_Close.clicked.connect(self.handle_Close)
 
 	def ScanButtonPressed(self, param):
 		pass
+
+	def handle_Calibrate(self):
+		print("Calibrate")
 
 	def handle_Close(self):
 		print("Sdfsdf")
