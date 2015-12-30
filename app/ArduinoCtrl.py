3b250272cf1be6bb8da0537cb011a747a08c3474
Mikhail Dmitrievskiy <mdmitr@gmail.com>
2015-12-30 08:06:36 +0500

    pep8

---
+++ app/ArduinoCtrl.py
@@ -0,0 +1,14 @@
+
+class ArduinoCtrl
+
+    def MoveToPoint(int x, int y):
+        return
+
+    def CalibrateTopLeft(int x, int y):
+        return false
+
+    def CalibrateBottomRight(int x, int y):
+        return false
+
+    def MeasureTemperature(int x, int y):
+        return 0
--- app/ThermalScan.py
+++ app/ThermalScan.py
@@ -6,61 +6,65 @@ import cv2
 
 import ui_ThermalScan
 
+
 class Ui_ThermalScanDialog(QtWidgets.QDialog, ui_ThermalScan.Ui_ThermalScanDialog):
 
-	def __init__(self, parent=None):
-		super(Ui_ThermalScanDialog, self).__init__(parent)
-		self.setupUi(self)
-		self.pushButton_Calibrate.clicked.connect(self.handle_Calibrate)
-		self.pushButton_Close.clicked.connect(self.handle_Close)
-		self.pushButton_Scan.clicked.connect(self.handle_Scan)
+    def __init__(self, parent=None):
+        super(Ui_ThermalScanDialog, self).__init__(parent)
+        self.setupUi(self)
+        self.pushButton_Calibrate.clicked.connect(self.handle_Calibrate)
+        self.pushButton_Close.clicked.connect(self.handle_Close)
+        self.pushButton_Scan.clicked.connect(self.handle_Scan)
 
-		image_file = r'../image_processing/edge_detection/pic3.jpg'
-		self.cvImage = cv2.imread(image_file)
-		height, width, bytesPerComponent = self.cvImage.shape
-		bytesPerLine = bytesPerComponent * width
+        image_file = r'../image_processing/edge_detection/pic3.jpg'
+        self.cvImage = cv2.imread(image_file)
+        height, width, bytesPerComponent = self.cvImage.shape
+        bytesPerLine = bytesPerComponent * width
 
-		self.cap = cv2.VideoCapture(0)
+        self.cap = cv2.VideoCapture(0)
 
-		cv2.cvtColor(self.cvImage, cv2.COLOR_BGR2RGB, self.cvImage)
-		self.mQImage = QImage(self.cvImage, width, height, bytesPerLine, QImage.Format_RGB888)
-		self.mQPixmap = QPixmap.fromImage(self.mQImage)
+        cv2.cvtColor(self.cvImage, cv2.COLOR_BGR2RGB, self.cvImage)
+        self.mQImage = QImage(self.cvImage, width, height,
+                              bytesPerLine, QImage.Format_RGB888)
+        self.mQPixmap = QPixmap.fromImage(self.mQImage)
 
-		self.mainImage.resizeEvent = self.imageResizeEvent
+        self.mainImage.resizeEvent = self.imageResizeEvent
 
-	def imageResizeEvent(self, event):
-		ww = self.mainImage.width()-2
-		hh = self.mainImage.height()-2
-		self.mainImage.setPixmap(self.mQPixmap.scaled(ww, hh, Qt.KeepAspectRatio))
+    def imageResizeEvent(self, event):
+        ww = self.mainImage.width() - 2
+        hh = self.mainImage.height() - 2
+        self.mainImage.setPixmap(
+            self.mQPixmap.scaled(ww, hh, Qt.KeepAspectRatio))
 
-	def handle_Scan(self, param):
-		if (self.cap.isOpened() == False):
-			return		
-		ret, frame = self.cap.read()
-		print(ret)
-		self.cvImage = frame
-		height, width, bytesPerComponent = self.cvImage.shape
-		bytesPerLine = bytesPerComponent * width
+    def handle_Scan(self, param):
+        if (self.cap.isOpened() == False):
+            return
+        ret, frame = self.cap.read()
+        print(ret)
+        self.cvImage = frame
+        height, width, bytesPerComponent = self.cvImage.shape
+        bytesPerLine = bytesPerComponent * width
 
-		cv2.cvtColor(self.cvImage, cv2.COLOR_BGR2RGB, self.cvImage)
-		self.mQImage = QImage(self.cvImage, width, height, bytesPerLine, QImage.Format_RGB888)
-		self.mQPixmap = QPixmap.fromImage(self.mQImage)
-		ww = self.mainImage.width()-2
-		hh = self.mainImage.height()-2
-		self.mainImage.setPixmap(self.mQPixmap.scaled(ww, hh, Qt.KeepAspectRatio))		
-		self.mainImage.repaint()
-		pass
+        cv2.cvtColor(self.cvImage, cv2.COLOR_BGR2RGB, self.cvImage)
+        self.mQImage = QImage(self.cvImage, width, height,
+                              bytesPerLine, QImage.Format_RGB888)
+        self.mQPixmap = QPixmap.fromImage(self.mQImage)
+        ww = self.mainImage.width() - 2
+        hh = self.mainImage.height() - 2
+        self.mainImage.setPixmap(
+            self.mQPixmap.scaled(ww, hh, Qt.KeepAspectRatio))
+        self.mainImage.repaint()
+        pass
 
-	def handle_Calibrate(self):
-		print("Calibrate")
+    def handle_Calibrate(self):
+        print("Calibrate")
 
-	def handle_Close(self):
+    def handle_Close(self):
 
-		ww = self.mainImage.width() - 2
-		hh = self.mainImage.height() - 2
-		self.mainImage.setPixmap(self.mQPixmap.scaled(ww, hh, Qt.KeepAspectRatio))
-		
-		self.mainImage.repaint()
-		print("Sdfsdf")
+        ww = self.mainImage.width() - 2
+        hh = self.mainImage.height() - 2
+        self.mainImage.setPixmap(
+            self.mQPixmap.scaled(ww, hh, Qt.KeepAspectRatio))
 
-	
+        self.mainImage.repaint()
+        print("Sdfsdf")
