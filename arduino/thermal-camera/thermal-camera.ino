9a43e1406cae3e838bd847cca160d45f28da2bad
Mikhail Dmitrievskiy <mdmitr@gmail.com>
2016-01-30 23:19:46 +0500

    adding main arduino sketch with simple servo commmands support

---
+++ arduino/thermal-camera/thermal-camera.ino
@@ -0,0 +1,52 @@
+/*
+Arduino Servo Test sketch
+*/
+#include <SoftwareSerial.h>
+#include "SerialCommand.h"
+#include <Servo.h>
+
+
+Servo servo_lr; // Define our Servo
+Servo servo_ud;
+SerialCommand cmd;
+
+void setup()
+{
+   servo_lr.attach(10); // servo on digital pin 10
+   servo_ud.attach(11);
+   Serial.begin(9600);
+
+   cmd.addCommand("lr", lr);
+   cmd.addCommand("ud", ud);
+   cmd.addCommand("rlr", rlr);
+   cmd.addCommand("rud", rud);
+}
+
+void loop()
+{
+  cmd.readSerial();
+}
+
+void lr()
+{
+  char * arg = cmd.next();
+  float ms = atof(arg);
+  servo_lr.writeMicroseconds(ms);
+}
+
+void ud()
+{
+  char * arg = cmd.next();
+  float ms = atof(arg);
+  servo_ud.writeMicroseconds(ms);
+}
+
+void rlr()
+{
+  Serial.println(servo_lr.readMicroseconds());
+}
+
+void rud()
+{
+  Serial.println(servo_ud.readMicroseconds());
+}
