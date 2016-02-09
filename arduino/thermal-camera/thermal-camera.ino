6d1ada4331cc092cd6d56dfeabb5637a0c543078
Mikhail Dmitrievskiy <mdmitr@gmail.com>
2016-02-09 08:18:29 +0500

    new optimized sketch for mg995 servos

--- arduino/thermal-camera/thermal-camera.ino
+++ arduino/thermal-camera/thermal-camera.ino
@@ -6,19 +6,36 @@ Arduino Servo Test sketch
 #include <Servo.h>
 #include <i2cmaster.h>
 
+#define LR_SERVO 9
+#define UD_SERVO 10
 
 Servo servo_lr; // Define our Servo
 Servo servo_ud;
 SerialCommand cmd;
 
+float servo_lr_ms;
+float servo_ud_ms;
+
+int servo_delay_per_ms = 4;
+int servo_min_delay = 20;
+int servo_max_delay = 1000;
+int sensor_delay = 50;
+
 void setup()
 {
-   servo_lr.attach(10, 1100, 1700); // servo on digital pin 10
-   servo_ud.attach(11, 550, 900);
+   servo_lr_ms = 1500;
+   servo_ud_ms = 1500;
+   servo_lr.writeMicroseconds(servo_lr_ms);
+   servo_lr.attach(LR_SERVO, 1100, 1700); // servo on digital pin 10
+   servo_ud.writeMicroseconds(servo_ud_ms);
+   servo_ud.attach(UD_SERVO);
+
+   delay(1000);
+   //servo_lr.detach();
+   //servo_ud.detach();
 
    i2c_init(); //Initialise the i2c bus
    PORTC = (1 << PORTC4) | (1 << PORTC5);//enable pullups
-
    
    Serial.begin(9600);
 
@@ -36,21 +53,49 @@ void loop()
 
 void lr()
 {
-  float prev_ms = servo_lr.readMicroseconds();
+  
+  if (servo_ud.attached())
+    servo_ud.detach();
+  /*
+  if (servo_lr.attached() == false)
+    servo_lr.attach(LR_SERVO);
+  */
   char * arg = cmd.next();
   float ms = atoi(arg);
+  if (ms == servo_lr_ms)
+  {
+    Serial.println("Ok");
+    return;
+  }
+  
   servo_lr.writeMicroseconds(ms);
-  delay(abs(ms-prev_ms)*1);
+  delay(max(servo_min_delay,min(servo_max_delay,abs(ms-servo_lr_ms)*servo_delay_per_ms)));
+  //servo_lr.detach();
+  servo_lr_ms = ms;
   Serial.println("Ok");
 }
 
 void ud()
 {
-  float prev_ms = servo_lr.readMicroseconds();
+  /*
+  if (servo_lr.attached())
+    servo_lr.detach();
+    */
+  if (servo_ud.attached() == false)
+    servo_ud.attach(UD_SERVO);
+  
   char * arg = cmd.next();
   float ms = atoi(arg);
+  if (ms == servo_ud_ms)
+  {
+    Serial.println("Ok");
+    return;
+  }
+  
   servo_ud.writeMicroseconds(ms);
-  delay(min(100,abs(ms-prev_ms)*5));
+  delay(max(servo_min_delay,min(servo_max_delay,abs(ms-servo_ud_ms)*servo_delay_per_ms)));
+  //servo_ud.detach();
+  servo_ud_ms = ms;
   Serial.println("Ok");
 }
 
@@ -93,8 +138,10 @@ void t()
     tempData = (tempData * tempFactor)-0.01;
     
     float celcius = tempData - 273.15;
+
+    delay(sensor_delay);
+    
     Serial.println("Ok");
-    //delay(20);
     Serial.println(celcius);
 }
 
